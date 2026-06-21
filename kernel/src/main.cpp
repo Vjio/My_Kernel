774845d28a90830d36d4a1c321fa9bd069973eab
Davidescu Victor <132272277+Vjio@users.noreply.github.com>
2026-06-21 11:45:35 +0300

    Initial commit

---
+++ kernel/src/main.cpp
@@ -0,0 +1,112 @@
+#include <cstdint>
+#include <cstddef>
+#include <limine.h>
+
+// Set the base revision to 6, this is recommended as this is the latest
+// base revision described by the Limine boot protocol specification.
+// See specification for further info.
+
+namespace {
+
+__attribute__((used, section(".limine_requests")))
+volatile std::uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
+
+}
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent, _and_ they should be accessed at least
+// once or marked as used with the "used" attribute as done here.
+
+namespace {
+
+__attribute__((used, section(".limine_requests")))
+volatile limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+    .revision = 0,
+    .response = nullptr
+};
+
+}
+
+// Finally, define the start and end markers for the Limine requests.
+// These can also be moved anywhere, to any .cpp file, as seen fit.
+
+namespace {
+
+__attribute__((used, section(".limine_requests_start")))
+volatile std::uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end")))
+volatile std::uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
+
+}
+
+// Halt and catch fire function.
+namespace {
+
+void hcf() {
+    for (;;) {
+#if defined (__x86_64__)
+        asm ("hlt");
+#elif defined (__aarch64__) || defined (__riscv)
+        asm ("wfi");
+#elif defined (__loongarch64)
+        asm ("idle 0");
+#endif
+    }
+}
+
+}
+
+// The following stubs are required by the Itanium C++ ABI (the one we use,
+// regardless of the "Itanium" nomenclature).
+// Like the memory functions above, these stubs can be moved to a different .cpp file,
+// but should not be removed, unless you know what you are doing.
+extern "C" {
+    int __cxa_atexit(void (*)(void *), void *, void *) { return 0; }
+    void __cxa_pure_virtual() { hcf(); }
+    void *__dso_handle;
+}
+
+// Extern declarations for global constructors array.
+extern void (*__init_array[])();
+extern void (*__init_array_end[])();
+
+// The following will be our kernel's entry point.
+// If renaming kmain() to something else, make sure to change the
+// linker script accordingly.
+extern "C" void kmain() {
+    // Ensure the bootloader actually understands our base revision (see spec).
+    if (LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision) == false) {
+        hcf();
+    }
+
+    // Call global constructors.
+    for (std::size_t i = 0; &__init_array[i] != __init_array_end; i++) {
+        __init_array[i]();
+    }
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == nullptr
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Print a nice pattern to screen as an example.
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    volatile std::uint32_t *fb_ptr = static_cast<volatile std::uint32_t *>(framebuffer->address);
+    for (std::size_t y = 0; y < framebuffer->height; y++) {
+        for (std::size_t x = 0; x < framebuffer->width; x++) {
+            std::uint32_t nX = x * 255 / framebuffer->width;
+            std::uint32_t nY = y * 255 / framebuffer->height;
+            fb_ptr[y * (framebuffer->pitch / 4) + x] = (nY << 8) | nX;
+        }
+    }
+
+    // We're done, just hang...
+    hcf();
+}
