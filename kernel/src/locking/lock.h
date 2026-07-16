e43c66a22e0389f6aa51ebdb56867c825af55db5
Davidescu Victor <davidescuvictor2004@gmail.com>
2026-07-16 11:59:56 +0300

    feat: add spinlock functions

---
+++ kernel/src/locking/lock.h
@@ -0,0 +1,16 @@
+#pragma once
+
+struct spinlock {
+    bool locked;
+};
+
+void acquire(struct spinlock *lock) {
+    while (__atomic_test_and_set(&lock->locked, __ATOMIC_ACQUIRE)) {
+        while (__atomic_load_n(&lock->locked, __ATOMIC_RELAXED))
+            __builtin_ia32_pause();
+    }
+}
+
+void release(struct spinlock *lock) {
+    __atomic_clear(&lock->locked, __ATOMIC_RELEASE);
+}
