3bc631f25c713b7760fa83099ab99ad603f637ec
Davidescu Victor <davidescuvictor2004@gmail.com>
2026-07-18 17:48:32 +0300

    chore: add new and delete operators

---
+++ kernel/src/operators.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <cstddef>
+#include "memory/heap.hpp"
+#include "stdio.hpp"
+
+void* operator new(size_t size) {
+    void* ptr = malloc(size);
+    if (!ptr) {
+        // this shouldn't happen
+        printf("new object creation failed!\n");
+    }
+    return ptr;
+}
+
+void* operator new[](size_t size) {
+    return operator new(size);
+}
+
+void operator delete(void* ptr) noexcept {
+    free(ptr);
+}
+
+void operator delete[](void* ptr) noexcept {
+    free(ptr);
+}
