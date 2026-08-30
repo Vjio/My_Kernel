635d154ee0dc43617155c981941d6305a19ce295
Davidescu Victor <davidescuvictor2004@gmail.com>
2026-08-30 15:33:43 +0300

    chore: add more operators

--- kernel/src/operators.hpp
+++ kernel/src/operators.hpp
@@ -23,3 +23,11 @@ void operator delete(void* ptr) noexcept {
 void operator delete[](void* ptr) noexcept {
     free(ptr);
 }
+
+void operator delete(void* ptr, size_t) noexcept {
+    free(ptr);
+}
+
+void operator delete[](void* ptr, size_t) noexcept {
+    free(ptr);
+}
